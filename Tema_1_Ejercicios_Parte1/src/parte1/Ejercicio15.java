e043ce0c8f1fc08d646a13ad81096ef7587e48ec
jemunoz <jemunoz@106-31.info2>
2024-09-25 09:22:08 +0200

    Ejercicio 15 terminado.

--- Tema_1_Ejercicios_Parte1/src/parte1/Ejercicio15.java
+++ Tema_1_Ejercicios_Parte1/src/parte1/Ejercicio15.java
@@ -5,16 +5,38 @@ import java.util.Scanner;
 public class Ejercicio15 {
 
 	public static void main(String[] args) {
-		
-		final double IVA = 21;
-		
+
+		// Constante que recoge el valor del IVA.
+		final double IVA = 0.21;
+
+		// Variable que recoge el precio del usuario.
 		double precioInicial;
-		
+
+		// Variable que recoge el aumento con el IVA.
+		double aumentoIva;
+
+		// Variable que recoge el precio con el IVA aplicado.
 		double precioFinal;
-		
-		Scanner sc = new Scanner (System.in);
-		
-		
+
+		// Creamos un nuevo Scanner.
+		Scanner sc = new Scanner(System.in);
+
+		// Solicitamos su precio al usuario.
+		System.out.println("Introduzca su precio inicial:");
+
+		// Leemos del teclado.
+		precioInicial = sc.nextDouble();
+
+		// Realizamos las operaciones correspondientes.
+		aumentoIva = precioInicial * IVA;
+
+		precioFinal = precioInicial + aumentoIva;
+
+		// Ofrecemos el resultado al usuario.
+		System.out.println("Su precio con el IVA aplicado es: " + precioFinal + " euros. ");
+
+		// Cerramos el Scanner.
+		sc.close();
 
 	}
 
